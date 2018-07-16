fb7512954102f70de65b65546408f2a2538c113e
gaolei <gaolei@ainirobot.com>
2018-07-16 11:27:44 +0800

    init demo

---
+++ DynamicLoadApp/app/src/main/java/com/ctrip/pluginapplication/MainActivity.java
@@ -0,0 +1,143 @@
+package com.ctrip.pluginapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * @author Zhenhua on 2018/3/7.
+ * @email zhshan@ctrip.com ^.^
+ */
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSION_REQUEST_CODE = 10000;
+    //    BottomNavigationView bottomNavigationView;
+    String[] permissionArray = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click();
+            }
+        });
+        findViewById(R.id.load).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                load();
+            }
+        });
+        checkPermission();
+        PluginManager.getInstance().setContext(this);
+
+
+    }
+
+    //事件绑定load
+    private void load() {
+        /**
+         * 事先放置到SD卡根目录的plugin.apk
+         * 现实场景中是有服务端下发
+         */
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/plugin_test", "plugin.apk");
+        PluginManager.getInstance().loadPath(file.getAbsoluteFile().getPath());
+    }
+
+    //事件绑定click
+    private void click() {
+        /**
+         * 点击跳往插件app的activity，一律跳转到PRoxyActivity
+         */
+        Intent intent = new Intent(this, ProxyActivity.class);
+        intent.putExtra("className", PluginManager.getInstance().getEntryName());
+        startActivity(intent);
+
+    }
+    public void checkPermission() {
+        /**
+         * 第 1 步: 检查是否有相应的权限
+         */
+        boolean isAllGranted = checkPermissionAllGranted(
+                permissionArray
+        );
+        // 如果这权限全都拥有, 则显示HomeFragment
+        if (isAllGranted) {
+//            switchFragment(0);
+            return;
+        }
+
+        /**
+         * 第 2 步: 请求权限
+         */
+        // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
+        ActivityCompat.requestPermissions(
+                this,
+                permissionArray,
+                MY_PERMISSION_REQUEST_CODE
+        );
+    }
+
+    /**
+     * 检查是否拥有指定的所有权限
+     */
+    private boolean checkPermissionAllGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 第 3 步: 申请权限结果返回处理
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            boolean isAllGranted = true;
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (isAllGranted) {
+                // 如果所有的权限都授予了, 则显示HomeFragment
+//                switchFragment(0);
+            } else {
+                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
+//                getAppDetailSettingIntent(this);
+                toast("App正常使用需要授权" );
+
+            }
+        }
+    }
+
+    public void toast(String content) {
+        Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+    }
+
+}
